b8c303418ba56bfcbec82e57bcaacfbc059d70b9
Luthfi <luthfisandi20@gmail.com>
2022-10-17 18:36:21 +0700

    challenge1

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,6 @@
+/*Buatlah sebuah fungsi yang dapat menjumlahkan semua parameter yang dimasukkan
+ke ddalam fungsi tersebut.*/
+
 function sum(){
     //write code here
   let amount = 0;
@@ -13,3 +16,9 @@ console.log(sum(1,2,7));
 console.log(sum(1,4));
 console.log(sum(11));
 console.log(sum(10,3,6,7,9));
+
+/*Output
+10
+5
+11
+35 */
