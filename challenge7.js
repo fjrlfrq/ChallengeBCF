8753c03e67728e007214470937f77828b7c76080
Luthfi <luthfisandi20@gmail.com>
2022-10-29 12:54:40 +0700

    challenge7

--- challenge7.js
+++ challenge7.js
@@ -5,6 +5,7 @@ function weirdMultiply(sentence) {
     //write your code
     let text = sentence.toString();
     let j = 1;
+    
     for(let i = 0; i < text.length; i++){
         j *= text[i];
     }
@@ -16,6 +17,6 @@ function weirdMultiply(sentence) {
 }
 
 console.log(weirdMultiply(39)); //-> 3 * 9 = 27 ->2 * 7 = 14 -> 1 * 4 = 4
-console.log(weirdMultiply(999)); //-> 9 * 9 * 9 =729 -> 7 * 2 * 9 =126 -> 1 * 2 * 6 = 12 -> 1 * 2 = 2
-console.log(weirdMultiply(3)); // -> 3 karena telah satu digit
+// console.log(weirdMultiply(999)); //-> 9 * 9 * 9 =729 -> 7 * 2 * 9 =126 -> 1 * 2 * 6 = 12 -> 1 * 2 = 2
+// console.log(weirdMultiply(3)); // -> 3 karena telah satu digit
 
