484023d2dab0e75932dae72d95d825b38a18c739
Luthfi <luthfisandi20@gmail.com>
2022-11-05 14:19:02 +0700

    challenge1 done

---
+++ CH11/challenge11.js
@@ -0,0 +1,42 @@
+/*
+Buatlah sebuah permainan tebak kata, gunakan file data json untuk
+menyimpan daftar pernyataan dan jawaban. file data json sudah disertakan di github.
+*/
+
+const fs = require('fs');
+const readline = require('node:readline');
+
+const data = fs.readFileSync('data.json', 'utf-8');
+const data1 = JSON.parse(data);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Tebakan: '
+});
+
+let tanya = 0;
+
+console.log('Selamat datang di permainan tebak kata, silahkan isi dengan jawaban yang benar ya!');
+console.log(`Pertanyaan: ${data1[tanya].definition}`);
+
+rl.prompt();
+rl.on('line', (line) => {
+    if (tanya < data1.length - 1) {
+        if (line.trim().toLowerCase() == data1[tanya].term) {
+            tanya++;
+            console.log('Selamat Anda Benar!');
+            console.log(`Pertanyaan: ${data1[tanya].definition}`);
+        } else {
+            console.log('Wkwkwkwk, Anda kurang beruntung!');
+        }
+    } else if (line.trim().toLowerCase() == data1[tanya].term) {
+        rl.close();
+        process.exit(0);
+    } else {
+        console.log("Wkwkwk anda kurang beruntung");
+    }
+    rl.prompt();
+}).on('close', () => {
+    console.log('Hore Anda Menang!');
+});
