7571c968ad6b43104dba2a520939cc3254db0d7d
Luthfi <luthfisandi20@gmail.com>
2022-11-05 14:20:36 +0700

    challenge1 is done

--- challenge1.js
+++ challenge1.js
@@ -1,7 +1,7 @@
 /*Buatlah sebuah fungsi yang dapat menjumlahkan semua parameter yang dimasukkan
 ke ddalam fungsi tersebut.*/
 
-zsum(){
+function sum(){
     //write code here
   let amount = 0;
     for (let j = 0; j < arguments.length; j++) {
